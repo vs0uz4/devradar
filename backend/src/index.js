3fc828b6f330a434aa28945b9a0661299cd5429d
vs0uz4 <vitor.rodrigues@gmail.com>
2020-02-10 02:04:07 -0300

    Install Mongoose and Prepare Backend for Use MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-cknei.mongodb.net/devradar?retryWrites=true&w=majority', {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.use(express.json())
 app.use(routes)
